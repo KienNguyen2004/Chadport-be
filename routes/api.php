f6e0485cd67bf65eff75b8fe11c2c59b9fac4ba6
KienNguyen2004 <kiennt18nd@gmail.com>
2024-11-01 10:24:10 +0700

    update route conflict

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,7 @@
 <?php
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
->>>>>>> 65f3f59 (update code)
-=======
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
->>>>>>> e8a8fe2 (Auth Admin)
 use App\Http\Controllers\Api\CategoryController;
-=======
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
->>>>>>> a50d287 (Auth Admin)
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
@@ -37,21 +23,7 @@ Route::group(['prefix' => 'admin'], function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::post('/refresh', [AuthController::class, 'refresh']);
         Route::post('/register', [AuthController::class, 'register']);
-<<<<<<< HEAD
     });
-=======
-    });  
-});
-
-// Group user routes
-Route::group(['prefix' => 'user'], function () {
-    Route::post('/login', [UserController::class, 'login']);
-    Route::group(['middleware' => ['auth:api', 'check.user.role']], function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::post('/refresh', [AuthController::class, 'refresh']);
-        Route::post('/register', [AuthController::class, 'register']);
-    });  
->>>>>>> a50d287 (Auth Admin)
 });
 
 // Group user routes
@@ -68,13 +40,4 @@ Route::post('products', [ProductController::class, 'create'])->name('products.in
 // Route::post('products', [ProductController::class, 'store'])->name('products.store');
 // Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
 // Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
-<<<<<<< HEAD
-=======
 Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
-<<<<<<< HEAD
->>>>>>> 4dc19ff559d11a13cbe8b4d8e3c17cc56c0fbd3f
-=======
-Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
->>>>>>> 65f3f59 (update code)
-=======
->>>>>>> e8a8fe2 (Auth Admin)
