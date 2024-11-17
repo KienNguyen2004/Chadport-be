c876e93dee508e8c7d9921a79b90139ab31920ac
HoangDai2 <tranminhhoangdai@gmail.com>
2024-11-17 16:47:53 +0700

    color-size

---
+++ database/migrations/2024_11_17_083643_update_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('col_id'); 
+            $table->unsignedBigInteger('size_id');
+            
+            $table->foreign('col_id')->references('id')->on('colors')->onDelete('cascade');
+            $table->foreign('size_id')->references('id')->on('sizes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('col_id');
+            $table->dropColumn('size_id');
+        });
+    }
+};
